19b7aed324386debda4ff5150bf557df9e04fd93
Peter Kim <seattlechem@yahoo.com>
2018-05-22 13:01:01 -0700

    askme_api/views.py initially added

--- askme/askme_api/views.py
+++ askme/askme_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+"""Rest Api views."""
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class AskViewApi(APIView):
+    """Using apiview."""
+
+    def get(self, request):
+        """Get request."""
+        return Response()
+
+    def post(self, request):
+        """Upload audio file."""
+
+        customHeader = request.META['HTTP_MYCUSTOMHEADER']
+        uploadedFile = open("file.wav", "wb")
+        uploadedFile.write(request.body)
+        uploadedFile.close()
+        return HttpResponse(escape(repr(request)))
