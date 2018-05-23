bdbc6414dc875adb1ecc0faade30e30ab72578ec
Peter Kim <seattlechem@yahoo.com>
2018-05-23 11:59:57 -0700

    absolute path implemented in rectotext.py

--- askme/askme/rectotext.py
+++ askme/askme/rectotext.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 
 
 def rec_to_text():
-    with open("askme_api/assets/text-recognition-67c11381cd20.json") as f:
+    with open(os.path.join(BASE_DIR, "askme_api/assets/text-recognition-67c11381cd20.json")) as f:
         GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
 
     r = sr.Recognizer()
@@ -14,6 +14,6 @@ def rec_to_text():
         audio = r.record(source)
     # Transcribe audio file
     text = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
-    with open("askme/assets/transcript.txt", "w") as f:
+    with open(os.path.join(BASE_DIR, "askme/assets/transcript.txt", "w")) as f:
         f.write(text)
     return text
