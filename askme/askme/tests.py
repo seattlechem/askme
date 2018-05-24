1e415e0f0cb2732b2d2a679fda6f0233b71c3d06
Peter Kim <seattlechem@yahoo.com>
2018-05-24 13:47:31 -0700

    route test cases

---
+++ askme/askme/tests.py
@@ -0,0 +1,21 @@
+"""Test cases for askme."""
+from django.test import TestCase
+
+
+class RouteTests(TestCase):
+    """Route test cases."""
+
+    def test_200_status_on_landing(self):
+        """Home page test case."""
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_200_status_on_aboutus(self):
+        """About us page test case."""
+        response = self.client.get('/aboutus')
+        self.assertEqual(response.status_code, 200)
+
+    def test_200_status_on_audio(self):
+        """Audio route test."""
+        response = self.client.get('/audio')
+        self.assertEqual(response.status_code, 200)
