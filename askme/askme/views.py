abb8f9e8aa9fbd9fc89197f3282044408939c003
andriiglukhyi <andrigluxii@gmail.com>
2018-05-24 13:24:24 -0700

    fixed bug with tooch

--- askme/askme/views.py
+++ askme/askme/views.py
@@ -20,20 +20,32 @@ def home_view(request):
 def save_view(request):
     try:
         f = request.FILES['data']
-        uploadedFile = open("askme/assets/file.wav", "wb")
-        uploadedFile.write(f.read())
-        uploadedFile.close()
-        question = rec_to_text()
-        answer = find(question)
-        tts = gTTS(text=answer, lang='en')
-        tts.save("askme/assets/good.mp3")
-        fname = "askme/assets/good.mp3"
-        f = open(fname, "rb") 
-        response = HttpResponse()
-        response.write(f.read())
-        response['Content-Type'] = 'audio/mp3'
-        response['Content-Length'] = os.path.getsize(fname)
-        return response
+        if len(f.read)>100:
+            uploadedFile = open("askme/assets/file.wav", "wb")
+            uploadedFile.write(f.read())
+            uploadedFile.close()
+            question = rec_to_text()
+            answer = find(question)
+            tts = gTTS(text=answer, lang='en')
+            tts.save("askme/assets/good.mp3")
+            fname = "askme/assets/good.mp3"
+            f = open(fname, "rb") 
+            response = HttpResponse()
+            response.write(f.read())
+            response['Content-Type'] = 'audio/mp3'
+            response['Content-Length'] = os.path.getsize(fname)
+            return response
+        else:
+            answer = "I'm sorry I have some connection Issue. Can You repeat Your question?"
+            tts = gTTS(text=answer, lang='en')
+            tts.save("askme/assets/good.mp3")
+            fname = "askme/assets/good.mp3"
+            f = open(fname, "rb") 
+            response = HttpResponse()
+            response.write(f.read())
+            response['Content-Type'] = 'audio/mp3'
+            response['Content-Length'] = os.path.getsize(fname)
+
     except KeyError:
         answer = "I'm sorry I have some connection Issue. Can You repeat Your question?"
         tts = gTTS(text=answer, lang='en')
--- chrome/options.js
+++ chrome/options.js
@@ -2,20 +2,61 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+// 'use strict';
 
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-constructOptions(kButtonColors);
+// let page = document.getElementById('buttonDiv');
+// const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+// function constructOptions(kButtonColors) {
+//   for (let item of kButtonColors) {
+//     let button = document.createElement('button');
+//     button.style.backgroundColor = item;
+//     button.addEventListener('click', function() {
+//       chrome.storage.sync.set({color: item}, function() {
+//         console.log('color is ' + item);
+//       })
+//     });
+//     page.appendChild(button);
+//   }
+// }
+// constructOptions(kButtonColors);
+
+
+
+// function mouseDown(){
+//   console.log('start')
+//   chrome.tabCapture.capture({audio: true}, (stream) => {
+//     let startTabId;
+//     chrome.tabs.query({active:true, currentWindow: true}, (tabs) => startTabId = tabs[0].id)
+//     const liveStream = stream;
+//     const audioCtx = new AudioContext();
+//     const source = audioCtx.createMediaStreamSource(stream);
+//     let mediaRecorder = new Recorder(source);
+//   })
+// }
+
+// function mouseUp(){
+//   console.log('stop')
+//   const stopCapture = function() {
+//     let endTabId;
+//     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+//       endTabId = tabs[0].id;
+//       if(mediaRecorder && startTabId === endTabId){
+//         mediaRecorder.stop();
+//         mediaRecorder.exportWAV((blob)=> {
+//           const audioURL = window.URL.createObjectURL(blob);
+//           const now = new Date(Date.now());
+//           const currentDate = now.toDateString();
+//           chrome.downloads.download({url: audioURL, filename: `${currentDate.replace(/\s/g, "-")} Capture`})
+//         })
+//       }});
+//     chrome.storage.sync.get({
+//       muteTab: false
+//     }, (options) => {
+//       if(!options.muteTab) {
+//         let audio = new Audio();
+//         audio.srcObject = liveStream;
+//         audio.play();
+//       }
+//     });
+//   };
+// }
